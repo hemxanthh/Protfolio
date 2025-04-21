51f6c1dcf3a8c9f37ae39368e3ba7aa56957deea
hemxanthh <hemanthkeerthi565@gmail.com>
2025-04-21 22:07:14 +0530

    contac info

---
+++ modal/contact.php
@@ -0,0 +1,29 @@
+<div class="contact-container">
+	  <h1>Contact Me</h1>
+	  <!-- Formspree integration -->
+	  <form action="https://formspree.io/f/xyzwgpgz" method="POST">
+		<div class="form-group">
+		  <label for="name">Your Name:</label>
+		  <input type="text" name="name" id="name" required>
+		</div>
+		
+		<div class="form-group">
+		  <label for="email">Your Email:</label>
+		  <input type="email" name="email" id="email" required>
+		</div>
+	
+		<div class="form-group">
+		  <label for="message">Your Message:</label>
+		  <textarea name="message" id="message" rows="4" required></textarea>
+		</div>
+	
+		<button type="submit">Send Message</button>
+	  </form>
+	
+	  <div class="form-message">
+		<!-- Success or error messages will be shown here -->
+	  </div>
+	</div>
+	
+	</body>
+	</html>
